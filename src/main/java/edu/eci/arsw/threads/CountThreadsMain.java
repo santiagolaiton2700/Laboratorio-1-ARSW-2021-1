a689fd1e3e91cfd40552bf20add943e9b0ee6fa8
Santiago Laiton <santiago.laiton@mail.escuelaing.edu.co>
2021-01-20 09:48:58 -0500

    inicio

---
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThreadsMain {
+    
+    public static void main(String a[]){
+
+        Thread hilo1 =new Thread(new CountThread(0,99));
+        Thread hilo2 =new Thread(new CountThread(99,199));
+        Thread hilo3 =new Thread(new CountThread(200,299));
+
+        hilo1.run();
+        hilo2.run();
+        hilo3.run();
+
+    }
+    
+}
